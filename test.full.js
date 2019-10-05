b1ada967a585d820a8126fa4ebe9b18aa123db84
Soldy <4786022+Soldy@users.noreply.github.com>
2019-10-05 07:10:24 +0100

    full test
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.full.js
+++ test.full.js
@@ -3,13 +3,15 @@ global.nanoTest   = new (require("nanoTest")).test();
 const firstTest   = require("./test/first.js").firstTest;
 const lineTests   = require("./test/line.js").lineTests;
 const tableTest   = require("./test/table.js").tableTest;
-const stringTest   = require("./test/string.js")stringTest;
+const graphTest   = require("./test/graph.js").graphTest;
+//const stringTest   = require("./test/string.js").stringTest;
 
 process.stdout.write("\u001b[2J\u001b[0;0f");
 
 
 firstTest();
 lineTests();
+graphTest();
 tableTest();
 
 global.nanoTest.run();
