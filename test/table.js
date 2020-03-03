38c55328c596757e076eedb7db9a35e87041ecd6
Soldy <4786022+Soldy@users.noreply.github.com>
2020-03-03 22:30:53 +0000

    cleaner table test
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test/table.js
+++ test/table.js
@@ -2,30 +2,37 @@
 
 const table = (require("../lib/table.js")).table;
 
+const input = [
+    ["testcolumA", "testcolumB", "testcolumC"], 
+    ["A0", "B0", "C0"], 
+    ["A1", "B1", "C1"], 
+    ["A2", "B2", "C2"]
+];
+
+
+const output = [
+    "┌──────────┬──────────┬──────────┐",
+    "│\u001b[1mtestcolumA\u001b[0m│\u001b[1mtestcolumB\u001b[0m│\u001b[1mtestcolumC\u001b[0m│",
+    "├──────────┼──────────┼──────────┤",
+    "│    A0    │    B0    │    C0    │",
+    "├──────────┼──────────┼──────────┤",
+    "│    A1    │    B1    │    C1    │",
+    "├──────────┼──────────┼──────────┤",
+    "│    A2    │    B2    │    C2    │",
+    "└──────────┴──────────┴──────────┘"
+];
+
 
 exports.tableTest=function(){
     global.nanoTest.add(
         "table",
         {
             "function":table,
-            "options":[[
-                ["testcolumA", "testcolumB", "testcolumC"], 
-                ["A0", "B0", "C0"], 
-                ["A1", "B1", "C1"], 
-                ["A2", "B2", "C2"]
-            ]]
+            "options":[
+                input
+            ]
         },
         "j==",
-        [
-             "┌──────────┬──────────┬──────────┐",
-             "│\u001b[1mtestcolumA\u001b[0m│\u001b[1mtestcolumB\u001b[0m│\u001b[1mtestcolumC\u001b[0m│",
-             "├──────────┼──────────┼──────────┤",
-             "│    A0    │    B0    │    C0    │",
-             "├──────────┼──────────┼──────────┤",
-             "│    A1    │    B1    │    C1    │",
-             "├──────────┼──────────┼──────────┤",
-             "│    A2    │    B2    │    C2    │",
-             "└──────────┴──────────┴──────────┘"
-        ]
+        output
     );
 };
