4b1a747a0874c3ebac4b134fe82d925ab33dbdca
Soldy <4786022+Soldy@users.noreply.github.com>
2020-08-20 20:45:00 +0100

    tet update
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ test/demo.table.js
@@ -0,0 +1,27 @@
+
+const nanoTest = new (require('nano-test')).test();
+const table = (require("./lib/table.js")).table;
+
+let input = [
+    ["testcolumA", "testcolumB", "testcolumC"], 
+    ["A0", "B0", "C0"], 
+    ["A1", "B1", "C1"], 
+    ["A2", "B2", "C2"]
+];
+
+
+let output = [
+    "┌──────────┬──────────┬──────────┐",
+    "│\u001b[1mtestcolumA\u001b[0m│\u001b[1mtestcolumB\u001b[0m│\u001b[1mtestcolumC\u001b[0m│",
+    "├──────────┼──────────┼──────────┤",
+    "│    A0    │    B0    │    C0    │",
+    "├──────────┼──────────┼──────────┤",
+    "│    A1    │    B1    │    C1    │",
+    "├──────────┼──────────┼──────────┤",
+    "│    A2    │    B2    │    C2    │",
+    "└──────────┴──────────┴──────────┘"
+];
+
+for (let i of table(input))
+    console.log(i);
+
